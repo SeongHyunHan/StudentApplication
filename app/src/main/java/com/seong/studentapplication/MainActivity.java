151bdc11dc87f43aba7e62ab3a728f515fed1f09
Seong Hyun Han <seonghyunhan7193@gmail.com>
2018-02-25 15:17:08 -0500

    CREATE AUTH PAGE

--- app/src/main/java/com/seong/studentapplication/MainActivity.java
+++ app/src/main/java/com/seong/studentapplication/MainActivity.java
@@ -2,12 +2,34 @@ package com.seong.studentapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnGetCode;
+    private EditText edtPhoneNumber;
+
+    private View.OnClickListener getAuth = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            Log.i("Auth", "Button Clicked");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        btnGetCode = new Button(this);
+        edtPhoneNumber = findViewById(R.id.edtPhoneNumber);
+
+        btnGetCode.setOnClickListener(getAuth);
         setContentView(R.layout.activity_main);
     }
 }
